ad462e28fa422c564ce5b17c6cbd135b5b68b106
Alexandre COULON <alex97.coul@gmail.com>
2022-04-09 19:50:28 +0200

    .

--- Assets/Scripts/BugController.cs
+++ Assets/Scripts/BugController.cs
@@ -4,11 +4,18 @@ namespace OneHourJam363
 {
     public class BugController : MonoBehaviour
     {
+        public float speed = 1f;
         private Rigidbody2D _rb;
+        private PlayerController _player;
 
         private void Start()
         {
-            
+            _rb.velocity = new Vector2(x * 10f, _rb.velocity.y);
+        }
+
+        private void Update()
+        {
+            Vector2 playerPos = PlayerController.Instance.transform.position;
         }
     }
 }
