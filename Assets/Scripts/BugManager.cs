fe5e240a8db5380956e547427cfaac198481e19c
Alexandre COULON <alex97.coul@gmail.com>
2022-04-09 19:43:22 +0200

    ..

--- Assets/Scripts/BugManager.cs
+++ Assets/Scripts/BugManager.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 namespace OneHourJam363
 {
     public class BugManager : MonoBehaviour
-{
-    public GameObject _bug;
-    private Vector2 _move;
-    private PlayerController _player;
-
-    void Start()
     {
-        Instantiate(_bug);
-    }
+        public float speed = 1f;
+        public GameObject _bug;
+        private Vector2 _move;
+        private PlayerController _player;
+        private Rigidbody2D _rb;
 
-    void Update()
-    {
-        Vector2 playerPos = PlayerController.Instance.transform.position;
-        Debug.Log(playerPos);
-    }
-}   
+        void Start()
+        {
+            Instantiate(_bug);
+        }
+
+        void Update()
+        {
+            Vector2 playerPos = PlayerController.Instance.transform.position;
+            Vector2 dir = playerPos - (Vector2)transform.position;
+
+
+
+            transform.Translate(dir.normalized * speed * Time.deltaTime);
+        }
+    }   
 }
 
