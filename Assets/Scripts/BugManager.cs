8e9181cb8771614b76c8a972e186bfdc84869d0d
Alexandre COULON <alex97.coul@gmail.com>
2022-04-09 19:34:41 +0200

    wip

--- Assets/Scripts/BugManager.cs
+++ Assets/Scripts/BugManager.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BugManager : MonoBehaviour
+namespace OneHourJam363
+{
+    public class BugManager : MonoBehaviour
 {
     public GameObject _bug;
     private Vector2 _move;
+    private PlayerController _player;
 
     void Start()
     {
+        _player = PlayerController.Instance;
+        
         Instantiate(_bug);
     }
 
     void Update()
     {
-        
+        Vector2 playerPos = _player.transform.position;
+        Debug.Log(playerPos);
     }
+}   
 }
+
